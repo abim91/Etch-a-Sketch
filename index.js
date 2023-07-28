b8cac30752092b5bd2e534aadb3a7d0d336deb66
Abim Shrestha <53064345+abim91@users.noreply.github.com>
2023-07-27 21:05:10 -0400

    changes for different values only

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function createGrid(number){
 const container = document.querySelector('.grid');//reference container id that already exists in html
-for(var i = 0; i < format ; i++){
+for(var i = 0; i < currentFormat ; i++){
     const row = document.createElement('div');// create new element
     row.classList.add = "row";//add content to the element
    // row.style.cssText = " border-color: black; background-color: red; width: 300px; height: 30px;";
     //document.body.appendChild(div); this works as well but the body is the parent
     container.appendChild(row);
-    for(var j = 0; j < format ; j++){// we are using each row box we create as a house/container for the rest of the boxes in column
+    for(var j = 0; j < currentFormat ; j++){// we are using each row box we create as a house/container for the rest of the boxes in column
         const column = document.createElement('div');
         column.classList.add("columns");
         column.style.cssText = " border-style: solid; border-width:0.25px 0.25px; border-color: black; background-color: #F9FAFB; width: 30px; height: 30px;";
@@ -16,11 +16,19 @@ for(var i = 0; i < format ; i++){
 }
 }
 
-var format = 4;
-createGrid(format);
+var currentFormat = 4;
+createGrid(currentFormat);
 document.getElementById("chooseGrid").addEventListener('click',function(e){
-   format= prompt("Select How many grid you want,up to 50");
-   createGrid(format);
+   var newFormat= prompt("Select How many grid you want,up to 50");
+  
+   if(newFormat != currentFormat){
+        createGrid(newFormat);
+      //  console.log('here');
+   }
+   else{
+        console.log("these are already equal silly");
+        return;
+   }
    
 });
 
